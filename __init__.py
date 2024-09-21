c241e91f339e127db2733cc67277418474da0b3f
AIFSH <1509359472@qq.com>
2024-09-21 08:12:52 +0800

    add audioslicer

--- __init__.py
+++ __init__.py
@@ -11,15 +11,60 @@ rmvpe_dir = os.path.join(ckpt_dir,"rmvpe")
 import torch
 import yaml
 import time
+import traceback
 import torchaudio
 from huggingface_hub import snapshot_download,hf_hub_download
 import torchaudio.compliance.kaldi as kaldi
+from slicer2 import Slicer
+import numpy as np
 from seedvc.modules.commons import recursive_munch,build_model,load_checkpoint           
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def adjust_f0_semitones(f0_sequence, n_semitones):
     factor = 2 ** (n_semitones / 12)
     return f0_sequence * factor
+
+class AudioSlicerNode:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required":{
+                "threshold":("INT",{
+                    "default": -40,
+                }),
+                "min_length":("INT",{
+                    "default": 5000,
+                }),
+                "min_interval":("INT",{
+                    "default": 300,
+                }),
+                "hop_size":("INT",{
+                    "default": 20,
+                }),
+                "max_sil_kept":("INT",{
+                    "default": 5000,
+                }),
+            }
+        }
+    RETURN_TYPES = ("SLICER",)
+    #RETURN_NAMES = ("image_output_name",)
+
+    FUNCTION = "slice"
+
+    #OUTPUT_NODE = False
+
+    CATEGORY = "AIFSH_SeedVC"
+
+    def slice(self,threshold,min_length,min_interval,hop_size,max_sil_kept):
+        slicer = Slicer(
+            threshold= threshold,
+            min_length= min_length,
+            min_interval= min_interval,
+            hop_size= hop_size,
+            max_sil_kept= max_sil_kept
+        )
+        return (slicer,)
+    
 
 class SeedVC4SingNode:
     def __init__(self):
@@ -52,6 +97,9 @@ class SeedVC4SingNode:
                 "n_quantizers":("INT",{
                     "default": 3
                 })
+            },
+            "optional":{
+                "slicer":("SLICER",)
             }
         }
     
@@ -75,96 +123,20 @@ class SeedVC4SingNode:
         print(f"from {sr} to {target_sr}")
         return waveform.numpy()[0]
 
-    def gen_audio(self,source,target,semi_tone_shift,diffusion_steps,
-                  length_adjust,inference_cfg_rate,n_quantizers):
-
-        dit_config_path = os.path.join(seedvc_dir,"config_dit_mel_seed_facodec_small_wavenet_f0.yml")
-        dit_checkpoint_path = os.path.join(seedvc_dir,"DiT_step_404000_seed_v2_uvit_facodec_small_wavenet_f0_pruned.pth")
-        
-        if self.model is None:
-            config_path = os.path.join(facodec_dir,"config.yml")
-            ckpt_path = os.path.join(facodec_dir, 'pytorch_model.bin')
-
-            codec_config = yaml.safe_load(open(config_path,"r"))
-            codec_model_params = recursive_munch(codec_config['model_params'])
-            codec_encoder = build_model(codec_model_params, stage="codec")
-
-            ckpt_params = torch.load(ckpt_path, map_location="cpu")
-
-            for key in codec_encoder:
-                codec_encoder[key].load_state_dict(ckpt_params[key], strict=False)
-            _ = [codec_encoder[key].eval() for key in codec_encoder]
-            _ = [codec_encoder[key].to(device) for key in codec_encoder]
-            self.codec_encoder = codec_encoder
-
-            # f0 conditioned model
-            config = yaml.safe_load(open(dit_config_path, 'r'))
-            model_params = recursive_munch(config['model_params'])
-            model_f0 = build_model(model_params, stage='DiT')
-            hop_length = config['preprocess_params']['spect_params']['hop_length']
-            self.sr = config['preprocess_params']['sr']
-
-            # Load checkpoints
-            model_f0, _, _, _ = load_checkpoint(model_f0, None, dit_checkpoint_path,
-                                            load_only_params=True, ignore_modules=[], is_distributed=False)
-            for key in model_f0:
-                model_f0[key].eval()
-                model_f0[key].to(device)
-            model_f0.cfm.estimator.setup_caches(max_batch_size=1, max_seq_length=8192)
-            self.model = model_f0
-            # f0 extractor
-            from seedvc.modules.rmvpe import RMVPE
-
-            self.rmvpe = RMVPE(os.path.join(rmvpe_dir,"rmvpe.pt"), is_half=False, device=device)
-
-            # Load additional modules
-            from seedvc.modules.campplus.DTDNN import CAMPPlus
-
-            self.campplus_model = CAMPPlus(feat_dim=80, embedding_size=192)
-            self.campplus_model.load_state_dict(torch.load(os.path.join(campplus_dir,"campplus_cn_common.bin"), map_location='cpu'))
-            self.campplus_model.eval()
-            self.campplus_model.to(device)
-
-            from seedvc.modules.hifigan.generator import HiFTGenerator
-            from seedvc.modules.hifigan.f0_predictor import ConvRNNF0Predictor
-
-            hift_checkpoint_path = os.path.join(seedvc_dir,"hift.pt")
-            hift_config_path = os.path.join(seedvc_dir,"hifigan.yml")
-                                                            
-            hift_config = yaml.safe_load(open(hift_config_path, 'r'))
-            self.hift_gen = HiFTGenerator(**hift_config['hift'], f0_predictor=ConvRNNF0Predictor(**hift_config['f0_predictor']))
-            self.hift_gen.load_state_dict(torch.load(hift_checkpoint_path, map_location='cpu'))
-            self.hift_gen.eval()
-            self.hift_gen.to(device)
-            
-            # Generate mel spectrograms
-            mel_fn_args = {
-                "n_fft": config['preprocess_params']['spect_params']['n_fft'],
-                "win_size": config['preprocess_params']['spect_params']['win_length'],
-                "hop_size": config['preprocess_params']['spect_params']['hop_length'],
-                "num_mels": config['preprocess_params']['spect_params']['n_mels'],
-                "sampling_rate": self.sr,
-                "fmin": 0,
-                "fmax": 8000,
-                "center": False
-            }
-            from seedvc.modules.audio import mel_spectrogram
-
-            self.to_mel = lambda x: mel_spectrogram(x, **mel_fn_args)
+    def get_single_audio(self,source_audio,ref_audio,diffusion_steps,
+                         inference_cfg_rate,semi_tone_shift,n_quantizers,
+                         length_adjust=1.0):
         sr = self.sr
-        source_audio = self.cfy2librosa(source,sr)
-        ref_audio = self.cfy2librosa(target,sr)
-
         source_audio = source_audio[:sr * 30]
         source_audio = torch.tensor(source_audio).unsqueeze(0).float().to(device)
 
+        ref_audio = ref_audio[:sr*30]
         ref_audio = ref_audio[:(sr * 30 - source_audio.size(-1))]
         ref_audio = torch.tensor(ref_audio).unsqueeze(0).float().to(device)
 
         source_waves_16k = torchaudio.functional.resample(source_audio, sr, 16000)
         ref_waves_16k = torchaudio.functional.resample(ref_audio, sr, 16000)
 
-        print(f"from {sr} to 16000")
         with torch.no_grad():
             converted_waves_24k = torchaudio.functional.resample(source_audio, sr, 24000)
             wave_lengths_24k = torch.LongTensor([converted_waves_24k.size(1)]).to(converted_waves_24k.device)
@@ -240,17 +212,147 @@ class SeedVC4SingNode:
 
             
 
-            time_vc_start = time.time()
+            
             vc_target = self.model.cfm.inference(cat_condition, torch.LongTensor([cat_condition.size(1)]).to(mel2.device), mel2, style2, None, diffusion_steps, inference_cfg_rate=inference_cfg_rate)
             vc_target = vc_target[:, :, mel2.size(-1):]
             vc_wave = self.hift_gen.inference(vc_target)
-            time_vc_end = time.time()
-            print(f"RTF: {(time_vc_end - time_vc_start) / vc_wave.size(-1) * sr}")
+        return vc_wave
+    
+    def gen_audio(self,source,target,semi_tone_shift,diffusion_steps,
+                  length_adjust,inference_cfg_rate,n_quantizers,slicer=None):
 
-        res_audio = {
-            "waveform": vc_wave.cpu().unsqueeze(0),
-            "sample_rate": sr
-        }
+        dit_config_path = os.path.join(seedvc_dir,"config_dit_mel_seed_facodec_small_wavenet_f0.yml")
+        dit_checkpoint_path = os.path.join(seedvc_dir,"DiT_step_404000_seed_v2_uvit_facodec_small_wavenet_f0_pruned.pth")
+        
+        if self.model is None:
+            config_path = os.path.join(facodec_dir,"config.yml")
+            ckpt_path = os.path.join(facodec_dir, 'pytorch_model.bin')
+
+            codec_config = yaml.safe_load(open(config_path,"r"))
+            codec_model_params = recursive_munch(codec_config['model_params'])
+            codec_encoder = build_model(codec_model_params, stage="codec")
+
+            ckpt_params = torch.load(ckpt_path, map_location="cpu")
+
+            for key in codec_encoder:
+                codec_encoder[key].load_state_dict(ckpt_params[key], strict=False)
+            _ = [codec_encoder[key].eval() for key in codec_encoder]
+            _ = [codec_encoder[key].to(device) for key in codec_encoder]
+            self.codec_encoder = codec_encoder
+
+            # f0 conditioned model
+            config = yaml.safe_load(open(dit_config_path, 'r'))
+            model_params = recursive_munch(config['model_params'])
+            model_f0 = build_model(model_params, stage='DiT')
+            hop_length = config['preprocess_params']['spect_params']['hop_length']
+            self.sr = config['preprocess_params']['sr']
+
+            # Load checkpoints
+            model_f0, _, _, _ = load_checkpoint(model_f0, None, dit_checkpoint_path,
+                                            load_only_params=True, ignore_modules=[], is_distributed=False)
+            for key in model_f0:
+                model_f0[key].eval()
+                model_f0[key].to(device)
+            model_f0.cfm.estimator.setup_caches(max_batch_size=1, max_seq_length=8192)
+            self.model = model_f0
+            # f0 extractor
+            from seedvc.modules.rmvpe import RMVPE
+
+            self.rmvpe = RMVPE(os.path.join(rmvpe_dir,"rmvpe.pt"), is_half=False, device=device)
+
+            # Load additional modules
+            from seedvc.modules.campplus.DTDNN import CAMPPlus
+
+            self.campplus_model = CAMPPlus(feat_dim=80, embedding_size=192)
+            self.campplus_model.load_state_dict(torch.load(os.path.join(campplus_dir,"campplus_cn_common.bin"), map_location='cpu'))
+            self.campplus_model.eval()
+            self.campplus_model.to(device)
+
+            from seedvc.modules.hifigan.generator import HiFTGenerator
+            from seedvc.modules.hifigan.f0_predictor import ConvRNNF0Predictor
+
+            hift_checkpoint_path = os.path.join(seedvc_dir,"hift.pt")
+            hift_config_path = os.path.join(seedvc_dir,"hifigan.yml")
+                                                            
+            hift_config = yaml.safe_load(open(hift_config_path, 'r'))
+            self.hift_gen = HiFTGenerator(**hift_config['hift'], f0_predictor=ConvRNNF0Predictor(**hift_config['f0_predictor']))
+            self.hift_gen.load_state_dict(torch.load(hift_checkpoint_path, map_location='cpu'))
+            self.hift_gen.eval()
+            self.hift_gen.to(device)
+            
+            # Generate mel spectrograms
+            mel_fn_args = {
+                "n_fft": config['preprocess_params']['spect_params']['n_fft'],
+                "win_size": config['preprocess_params']['spect_params']['win_length'],
+                "hop_size": config['preprocess_params']['spect_params']['hop_length'],
+                "num_mels": config['preprocess_params']['spect_params']['n_mels'],
+                "sampling_rate": self.sr,
+                "fmin": 0,
+                "fmax": 8000,
+                "center": False
+            }
+            from seedvc.modules.audio import mel_spectrogram
+
+            self.to_mel = lambda x: mel_spectrogram(x, **mel_fn_args)
+        
+        sr = self.sr
+        source_audio = self.cfy2librosa(source,sr)
+        ref_audio = self.cfy2librosa(target,sr)
+
+        if slicer:
+            slicer.load_params(sr)
+            waves = []
+            chunks = slicer.slice(source_audio)
+            
+            for i, (chunk, start, end) in enumerate(chunks):
+                # tmp_max = np.abs(chunk).max()
+                # if(tmp_max>1):chunk/=tmp_max
+                # chunk = (chunk / tmp_max * (0.9 * 0.25)) + (1 - 0.25) * chunk
+                if i == 0:
+                    silent_wave = np.zeros((1,start))
+                    waves.append(silent_wave)
+                    print(silent_wave.shape)
+                time_vc_start = time.time()
+                chunk_duration = (end-start)//sr
+                try:
+                    fake_wave = self.get_single_audio(source_audio[start:end],ref_audio,diffusion_steps,
+                                                    inference_cfg_rate,semi_tone_shift,n_quantizers)
+                    time_vc_end = time.time()
+                    print(f"i {i} chunk duration:{chunk_duration} RTF: {(time_vc_end - time_vc_start) / fake_wave.size(-1) * sr}")
+                    waves.append(fake_wave.cpu().numpy())
+                    dur = end-start-fake_wave.size(-1)
+                    if dur > 0:
+                        silent_wave = np.zeros((1,dur))
+                        waves.append(silent_wave)
+                except:
+                    print(f"{chunk_duration} > 30s can't get fake, slient replace")
+                    silent_wave = np.zeros((1,end-start))
+                    waves.append(silent_wave)
+                    traceback.print_exc()
+
+                if i == len(chunks) - 1 and source_audio.shape[0]>end:
+                    silent_wave = np.zeros((1,source_audio.shape[0]-end))
+                    waves.append(silent_wave)
+
+            vc_wave = np.concatenate(waves,axis=-1)
+            # print(vc_wave.shape)
+            # assert vc_wave.shape[-1] == source_audio.shape[0], f"{vc_wave.shape} not = {source_audio.shape}"
+            res_audio = {
+                "waveform": torch.tensor(vc_wave).unsqueeze(0),
+                "sample_rate": sr
+            }
+        else:
+            time_vc_start = time.time()
+            vc_wave = self.get_single_audio(source_audio,ref_audio,diffusion_steps,
+                                        inference_cfg_rate,semi_tone_shift,n_quantizers,
+                                        length_adjust)
+            time_vc_end = time.time()
+            print(f"RTF: {(time_vc_end - time_vc_start) / fake_wave.size(-1) * sr}")
+                    
+            res_audio = {
+                "waveform": vc_wave.unsqueeze(0),
+                "sample_rate": sr
+            }
         return (res_audio, )
 
 
@@ -479,6 +581,7 @@ class SeedVCNode:
         return (res_audio, )
 
 NODE_CLASS_MAPPINGS = {
+    "AudioSlicerNode":AudioSlicerNode,
     "SeedVC4SingNode":SeedVC4SingNode,
     "SeedVCNode": SeedVCNode
 }
---
+++ slicer2.py
@@ -0,0 +1,275 @@
+import numpy as np
+
+
+"""
+Threshold（阈值）：以 dB（分贝）表示的 RMS 阈值。所有 RMS 值都低于此阈值的区域将被视为静音。如果音频有噪音，增加此值。
+Minimum Length（最小长度）：以默认值 5000（毫秒为单位）为例，简单来说就是切割后的每个音频片段都不少于 5 秒。
+Minimum Interval（最小间距）：以默认值 300（毫秒为单位）为例，简单来说就是少于 0.3 秒的静音不会被切割丢掉，超过 0.3 秒的静音部分才丢掉。如果音频仅包含短暂的中断，请将此值设置得更小。此值越小，此应用程序可能生成的切片音频剪辑就越多。请注意，此值必须小于 minimum length 且大于 hop size。
+Hop Size（跳跃步长）：每个 RMS 帧的长度（说白了就是精度），以毫秒为单位。增加此值将提高切片的精度，但会降低处理速度。默认值为 10。
+Maximum Silence Length（最大静音长度）：在切片音频周围保持的最大静音长度，以毫秒为单位。根据需要调整此值。请注意，设置此值并不意味着切片音频中的静音部分具有完全给定的长度。如上所述，该算法将搜索要切片的最佳位置。默认值为 1000。
+"""
+# This function is obtained from librosa.
+def get_rms(
+    y,
+    frame_length=2048,
+    hop_length=512,
+    pad_mode="constant",
+):
+    padding = (int(frame_length // 2), int(frame_length // 2))
+    y = np.pad(y, padding, mode=pad_mode)
+
+    axis = -1
+    # put our new within-frame axis at the end for now
+    out_strides = y.strides + tuple([y.strides[axis]])
+    # Reduce the shape on the framing axis
+    x_shape_trimmed = list(y.shape)
+    x_shape_trimmed[axis] -= frame_length - 1
+    out_shape = tuple(x_shape_trimmed) + tuple([frame_length])
+    xw = np.lib.stride_tricks.as_strided(y, shape=out_shape, strides=out_strides)
+    if axis < 0:
+        target_axis = axis - 1
+    else:
+        target_axis = axis + 1
+    xw = np.moveaxis(xw, -1, target_axis)
+    # Downsample along the target axis
+    slices = [slice(None)] * xw.ndim
+    slices[axis] = slice(0, None, hop_length)
+    x = xw[tuple(slices)]
+
+    # Calculate power
+    power = np.mean(np.abs(x) ** 2, axis=-2, keepdims=True)
+
+    return np.sqrt(power)
+
+
+class Slicer:
+    def __init__(
+        self,
+        sr: int = 22050,
+        threshold: float = -40.0,
+        min_length: int = 5000,
+        min_interval: int = 300,
+        hop_size: int = 20,
+        max_sil_kept: int = 5000,
+    ):
+        
+        if not min_length >= min_interval >= hop_size:
+            raise ValueError(
+                "The following condition must be satisfied: min_length >= min_interval >= hop_size"
+            )
+        if not max_sil_kept >= hop_size:
+            raise ValueError(
+                "The following condition must be satisfied: max_sil_kept >= hop_size"
+            )
+        self.threshold = 10 ** (threshold / 20.0)
+        self.hop_size = hop_size
+        self.min_length = min_length
+        self.min_interval = min_interval
+        self.max_sil_kept = max_sil_kept
+    
+    def load_params(self,sr):
+        min_interval = sr * self.min_interval / 1000
+        self.hop_size = round(sr * self.hop_size / 1000)
+        self.win_size = min(round(min_interval), 4 * self.hop_size)
+        self.min_length = round(sr * self.min_length / 1000 / self.hop_size)
+        self.min_interval = round(min_interval / self.hop_size)
+        self.max_sil_kept = round(sr * self.max_sil_kept / 1000 / self.hop_size)
+
+    def _apply_slice(self, waveform, begin, end):
+        if len(waveform.shape) > 1:
+            return waveform[
+                :, begin * self.hop_size : min(waveform.shape[1], end * self.hop_size)
+            ]
+        else:
+            return waveform[
+                begin * self.hop_size : min(waveform.shape[0], end * self.hop_size)
+            ]
+
+    # @timeit
+    def slice(self, waveform):
+        if len(waveform.shape) > 1:
+            samples = waveform.mean(axis=0)
+        else:
+            samples = waveform
+        if samples.shape[0] <= self.min_length:
+            return [waveform]
+        rms_list = get_rms(
+            y=samples, frame_length=self.win_size, hop_length=self.hop_size
+        ).squeeze(0)
+        sil_tags = []
+        silence_start = None
+        clip_start = 0
+        for i, rms in enumerate(rms_list):
+            # Keep looping while frame is silent.
+            if rms < self.threshold:
+                # Record start of silent frames.
+                if silence_start is None:
+                    silence_start = i
+                continue
+            # Keep looping while frame is not silent and silence start has not been recorded.
+            if silence_start is None:
+                continue
+            # Clear recorded silence start if interval is not enough or clip is too short
+            is_leading_silence = silence_start == 0 and i > self.max_sil_kept
+            need_slice_middle = (
+                i - silence_start >= self.min_interval
+                and i - clip_start >= self.min_length
+            )
+            if not is_leading_silence and not need_slice_middle:
+                silence_start = None
+                continue
+            # Need slicing. Record the range of silent frames to be removed.
+            if i - silence_start <= self.max_sil_kept:
+                pos = rms_list[silence_start : i + 1].argmin() + silence_start
+                if silence_start == 0:
+                    sil_tags.append((0, pos))
+                else:
+                    sil_tags.append((pos, pos))
+                clip_start = pos
+            elif i - silence_start <= self.max_sil_kept * 2:
+                pos = rms_list[
+                    i - self.max_sil_kept : silence_start + self.max_sil_kept + 1
+                ].argmin()
+                pos += i - self.max_sil_kept
+                pos_l = (
+                    rms_list[
+                        silence_start : silence_start + self.max_sil_kept + 1
+                    ].argmin()
+                    + silence_start
+                )
+                pos_r = (
+                    rms_list[i - self.max_sil_kept : i + 1].argmin()
+                    + i
+                    - self.max_sil_kept
+                )
+                if silence_start == 0:
+                    sil_tags.append((0, pos_r))
+                    clip_start = pos_r
+                else:
+                    sil_tags.append((min(pos_l, pos), max(pos_r, pos)))
+                    clip_start = max(pos_r, pos)
+            else:
+                pos_l = (
+                    rms_list[
+                        silence_start : silence_start + self.max_sil_kept + 1
+                    ].argmin()
+                    + silence_start
+                )
+                pos_r = (
+                    rms_list[i - self.max_sil_kept : i + 1].argmin()
+                    + i
+                    - self.max_sil_kept
+                )
+                if silence_start == 0:
+                    sil_tags.append((0, pos_r))
+                else:
+                    sil_tags.append((pos_l, pos_r))
+                clip_start = pos_r
+            silence_start = None
+        # Deal with trailing silence.
+        total_frames = rms_list.shape[0]
+        if (
+            silence_start is not None
+            and total_frames - silence_start >= self.min_interval
+        ):
+            silence_end = min(total_frames, silence_start + self.max_sil_kept)
+            pos = rms_list[silence_start : silence_end + 1].argmin() + silence_start
+            sil_tags.append((pos, total_frames + 1))
+        # Apply and return slices.
+        ####音频+起始时间+终止时间
+        if len(sil_tags) == 0:
+            return [[waveform,0,int(total_frames*self.hop_size)]]
+        else:
+            chunks = []
+            if sil_tags[0][0] > 0:
+                chunks.append([self._apply_slice(waveform, 0, sil_tags[0][0]),0,int(sil_tags[0][0]*self.hop_size)])
+            for i in range(len(sil_tags) - 1):
+                chunks.append(
+                    [self._apply_slice(waveform, sil_tags[i][1], sil_tags[i + 1][0]),int(sil_tags[i][1]*self.hop_size),int(sil_tags[i + 1][0]*self.hop_size)]
+                )
+            if sil_tags[-1][1] < total_frames:
+                chunks.append(
+                    [self._apply_slice(waveform, sil_tags[-1][1], total_frames),int(sil_tags[-1][1]*self.hop_size),int(total_frames*self.hop_size)]
+                )
+            return chunks
+
+
+def main():
+    import os.path
+    from argparse import ArgumentParser
+
+    import librosa
+    import soundfile
+
+    parser = ArgumentParser()
+    parser.add_argument("audio", type=str, help="The audio to be sliced")
+    parser.add_argument(
+        "--out", type=str, help="Output directory of the sliced audio clips"
+    )
+    parser.add_argument(
+        "--db_thresh",
+        type=float,
+        required=False,
+        default=-40,
+        help="The dB threshold for silence detection",
+    )
+    parser.add_argument(
+        "--min_length",
+        type=int,
+        required=False,
+        default=5000,
+        help="The minimum milliseconds required for each sliced audio clip",
+    )
+    parser.add_argument(
+        "--min_interval",
+        type=int,
+        required=False,
+        default=300,
+        help="The minimum milliseconds for a silence part to be sliced",
+    )
+    parser.add_argument(
+        "--hop_size",
+        type=int,
+        required=False,
+        default=10,
+        help="Frame length in milliseconds",
+    )
+    parser.add_argument(
+        "--max_sil_kept",
+        type=int,
+        required=False,
+        default=500,
+        help="The maximum silence length kept around the sliced clip, presented in milliseconds",
+    )
+    args = parser.parse_args()
+    out = args.out
+    if out is None:
+        out = os.path.dirname(os.path.abspath(args.audio))
+    audio, sr = librosa.load(args.audio, sr=None, mono=False)
+    slicer = Slicer(
+        sr=sr,
+        threshold=args.db_thresh,
+        min_length=args.min_length,
+        min_interval=args.min_interval,
+        hop_size=args.hop_size,
+        max_sil_kept=args.max_sil_kept,
+    )
+    chunks = slicer.slice(audio)
+    if not os.path.exists(out):
+        os.makedirs(out)
+    for i, chunk in enumerate(chunks):
+        if len(chunk.shape) > 1:
+            chunk = chunk.T
+        soundfile.write(
+            os.path.join(
+                out,
+                f"%s_%d.wav"
+                % (os.path.basename(args.audio).rsplit(".", maxsplit=1)[0], i),
+            ),
+            chunk,
+            sr,
+        )
+
+
+if __name__ == "__main__":
+    main()
